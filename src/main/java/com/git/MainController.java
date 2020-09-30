f93e62f2c98be939abaabb08bdefee8ca3d99e97
Martin Quiroga <martin.quiroga.05@gmail.com>
2020-09-30 20:55:15 -0300

    agregué método solicitado reqMap02

--- src/main/java/com/git/MainController.java
+++ src/main/java/com/git/MainController.java
@@ -1,5 +1,6 @@
 package com.git;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class MainController {
     @RequestMapping(path = "/metodo1") //cuando escriba en el browser ese contenido me meustras el metodo
     public String reqMap01(){
         return "metodo1";
+    }
+
+    @RequestMapping(path = "/metodo2/{parametro1}")
+    public String reqMap02(@PathVariable String parametro1) {
+        return parametro1;
     }
 }
